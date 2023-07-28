cd43617898c34ef8d5852f115c471318f03314ea
Ben Mitchinson <mitchinson.dev@gmail.com>
2023-07-27 23:24:25 -0500

    Add autocomplete component for category + split up into components

---
+++ src/routes/entry/autocomplete.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import AutoComplete from "simple-svelte-autocomplete";
+
+  export let selectedCategory: String;
+
+  const categoryChoices = ["Gas", "Grocery", "Date Night"];
+</script>
+
+<!-- is there a way fail validation if value isn't in item list?  -->
+<!-- right now you can select, enter no match text, and it will submit the last entry selected -->
+<!-- aka: entering no match text doesn't clear any existing selections -->
+
+<label for="category">Category</label>
+<AutoComplete
+  items={categoryChoices}
+  bind:selectedItem={selectedCategory}
+  noInputStyles={true}
+  hideArrow
+  createText="No Match"
+  inputId="category-select"
+  required
+/>
+
+<style lang="scss">
+  :global(.autocomplete) {
+    min-width: 0em !important;
+    height: 1.2em !important;
+  }
+</style>
---
+++ src/routes/entry/form.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+  import { createForm } from "svelte-forms-lib";
+  import { onMount } from "svelte";
+  import { App } from "@capacitor/app";
+  import Autocomplete from "./autocomplete.svelte";
+
+  const { form, handleChange, handleSubmit } = createForm({
+    initialValues: {
+      price: "",
+      category: "",
+      description: "",
+      // datetime: new Date(),
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values));
+    },
+  });
+
+  onMount(() => {
+    document.getElementById("price")?.focus();
+    App.addListener("appStateChange", ({ isActive }) => {
+      isActive && document.getElementById("price")?.focus();
+    });
+  });
+</script>
+
+<div class="entry-form">
+  <form on:submit={handleSubmit}>
+    <div class="row-item space-between">
+      <label for="price">Price</label>
+      <input
+        id="price"
+        name="price"
+        type="number"
+        step=".01"
+        inputmode="decimal"
+        on:change={handleChange}
+        bind:value={$form.price}
+      />
+    </div>
+
+    <div class="row-item space-between">
+      <label for="description">Description</label>
+      <input
+        id="description"
+        name="description"
+        on:change={handleChange}
+        bind:value={$form.description}
+      />
+    </div>
+
+    <div class="row-item space-between">
+      <Autocomplete bind:selectedCategory={$form.category} />
+    </div>
+
+    <div class="row-item center">
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+</div>
+
+<style lang="scss">
+  .entry-form {
+    font-size: 1.3rem;
+  }
+  .row-item {
+    display: flex;
+    margin-bottom: 1em;
+    label {
+      margin-right: 1em;
+    }
+  }
+  button {
+    padding: 1em 2em;
+    -webkit-appearance: none;
+  }
+</style>
